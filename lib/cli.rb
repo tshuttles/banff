e9d929dfe45b88dc160530e25e3ea311d10331e7
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-14 17:37:15 +0000

    set up cli.rb

---
+++ lib/cli.rb
@@ -0,0 +1,21 @@
+class CLI::Banff 
+  
+  def call 
+    list_attractions 
+    menu 
+    goodbye
+  end 
+  
+  def list_attractions
+    puts "Hello! Here are Banff's greatest attractions."
+    lists the attractions with scraper 
+  end 
+  
+  def menu 
+    input = nil 
+    while input != "exit"
+      puts "Enter the number of the attraction you are most interested in."
+    lists the input code 
+  end 
+  
+end 
