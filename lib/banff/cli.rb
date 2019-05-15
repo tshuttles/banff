71ab0065c562f5eac3df85ab8d5623981de556ce
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-15 22:05:59 +0000

    update CLI

--- lib/banff/cli.rb
+++ lib/banff/cli.rb
@@ -7,19 +7,20 @@ class CLI
   end 
   
   def list_attractions
-    puts "Hello! Here are Banff's greatest attractions."
+    puts "Hello! Here are Banff's greatest attractions." 
     # lists the attractions with scraper 
   end 
   
   def menu 
     input = nil 
     while input != "exit"
-      puts "Enter the number of the attraction you are most interested in."
+      puts "Enter the number of the attraction you are most interested in, or type exit to exit."
     # lists the input code 
     end 
   end 
   
   def goodbye
+    puts "Thank you! We hope to see you return soon for more information on Banff's world-class attractions!"
   end 
   
 end 
--- lib/banff/scraper.rb
+++ lib/banff/scraper.rb
@@ -1,8 +1,8 @@
 class Scraper 
   
   def self.scrape_attractions
-    doc = Nokogiri::HTML(open("https://www.tripadvisor.com/Attractions-g154911-Activities-Banff_Banff_National_Park_Alberta.html"))
-    attractions = [] 
+    # doc = Nokogiri::HTML(open("https://www.tripadvisor.com/Attractions-g154911-Activities-Banff_Banff_National_Park_Alberta.html"))
+    # attractions = [] 
     
     # doc.css("").each do |attractions| 
     #   attractions_hash = {
