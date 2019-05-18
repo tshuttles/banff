9eaeb2c3d36d6ec04799a98cf5f53de292077125
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-18 18:15:06 +0000

    update scraper class

--- lib/banff/scraper.rb
+++ lib/banff/scraper.rb
@@ -1,9 +1,23 @@
 class Scraper 
   
-  def self.scrape
-    attraction = [] 
-    attraction << self.scrape_attraction
-    attraction 
+  def get_page 
+    Nokogiri::HTML(open("https://www.tripadvisor.com/Attractions-g154911-Activities-Banff_Banff_National_Park_Alberta.html"))
+  end 
+  
+  def scrape_attraction_index 
+    self.get_page.css("#FILTERED_LIST li")
+  end 
+  
+  def make_attractions
+    scrape_attraction_index.each do |doc|
+      Attraction.new_from_index_page(doc)
+    end
+  end
+  
+  # def self.scrape
+  #   attraction = [] 
+  #   attraction << self.scrape_attraction
+  #   attraction 
     
     
     # attraction = Attraction.new 
@@ -26,17 +40,17 @@ class Scraper
     #     attractions << attractions_hash 
     # end 
     # attractions
-  end 
+#   end 
   
-  def self.scrape_attraction
-    doc = Nokogiri::HTML(open("https://www.tripadvisor.com/Attractions-g154911-Activities-Banff_Banff_National_Park_Alberta.html"))
-    attraction = self.new 
-    binding.pry 
-    attraction.name = doc.css("#FILTERED_LIST li")[0].css("a")[2].text
-    attraction.type = doc.css("#FILTERED_LIST li")[0].css("span")[9].text
-    attraction.price = doc.css("#FILTERED_LIST li")[0].css("span")[13].text
-    attraction.url = doc.css("#FILTERED_LIST li")[0].css("a")[2].attr("href")
+#   def self.scrape_attraction
+#     doc = Nokogiri::HTML(open("https://www.tripadvisor.com/Attractions-g154911-Activities-Banff_Banff_National_Park_Alberta.html"))
+#     attraction = self.new 
+#     binding.pry 
+#     attraction.name = doc.css("#FILTERED_LIST li")[0].css("a")[2].text
+#     attraction.type = doc.css("#FILTERED_LIST li")[0].css("span")[9].text
+#     attraction.price = doc.css("#FILTERED_LIST li")[0].css("span")[13].text
+#     attraction.url = doc.css("#FILTERED_LIST li")[0].css("a")[2].attr("href")
     
-    attraction 
-  end 
+#     attraction 
+#   end 
 end 
