7a7899a5d3bdc25329fbd8dad2e12e1e8fe5dd6e
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-18 18:28:25 +0000

    update attraction class

--- lib/banff/attraction.rb
+++ lib/banff/attraction.rb
@@ -1,14 +1,28 @@
 class Attraction 
   
-  attr_accessor :name, :type, :price, :url 
+  attr_accessor :name, :type, :price, :url, :description, :open_hours, :duration
   
-  @@attractions = []
+  @@all = []
   
-  def initialize
+  def self.new_from_index_page(doc)
+    self.new(
+      doc.css("#FILTERED_LIST li")[0].css("a")[2].text,
+      doc.css("#FILTERED_LIST li")[0].css("span")[9].text,
+      doc.css("#FILTERED_LIST li")[0].css("span")[13].text,
+      doc.css("#FILTERED_LIST li")[0].css("a")[2].attr("href")
+      )
+  end
+  
+  def initialize(name=nil, type=nil, price=nil, url=nil)
+    @name = name
+    @type = type 
+    @price = price 
+    @url = url
+    @@all << self
   end 
   
   def self.all
-    @@attractions
+    @@all 
   end 
   
   
