6423db180d0216603444af4a48485fe809d09453
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-19 05:24:44 +0000

    change CLI class

--- lib/banff/cli.rb
+++ lib/banff/cli.rb
@@ -1,40 +1,72 @@
 class CLI
   
   def call 
+    # binding.pry
+    Scraper.new.make_attractions 
+    start 
+    
     # list
-    binding.pry
-    menu 
-    goodbye
+    # menu 
+    # goodbye
   end 
   
-  def list
-    # lists the attractions with scraper 
+  def start 
+    puts ""
     puts "Hello! Here are Banff's greatest attractions." 
-    @attractions = Attraction.all 
-    @attractions.each.with_index do |attraction, x|
+    input = gets.strip.to_i
+    
+    print_attractions(input)
+    
+    puts ""
+    puts "Which attraction would you like more information on?"
+    input = gets.strip
+    
+    
+  end 
+  
+  def print_attractions(from_number)
+    puts ""
+    puts "---------- Attractions #{from_number} - #{from_number+14} ----------"
+    puts ""
+    WorldsBestRestaurants::Attraction.all[from_number-1, 15].each.with_index(from_number) do |attraction, x|
       puts "#{x}. #{attraction.name} - #{attraction.type} - #{attraction.price}"
-    end 
-  end 
+    end
+  end
   
-  def menu 
-    input = nil 
-    while input != "exit"
-      puts "Enter the number of the attraction you are most interested in, or type in list to see the options again, or type exit."
-      input = gets.strip.downcase 
+  # def list
+  #   # lists the attractions with scraper 
+  #   puts ""
+  #   puts "Hello! Here are Banff's greatest attractions." 
+  #   @attractions = Attraction.all 
+  #   @attractions.each.with_index do |attraction, x|
+  #     puts "#{x}. #{attraction.name} - #{attraction.type} - #{attraction.price}"
+  #   end 
+  # end 
+  
+  # def menu 
+  #   input = nil 
+  #   while input != "exit"
+  #     puts "" 
+  #     puts "Enter the number of the attraction you are most interested in, or type in list to see the options again, or type exit."
+  #     input = gets.strip.downcase 
       
-      if input.to_i > 0 
-        the_attraction = @attractions[input.to_i-1]
-        puts "#{attraction.name} - #{attraction.type} - #{attraction.price}"
-      elsif input == "list" 
-        list 
-      else 
-        puts "Invalid entry. Please type a number for information on an attraction, 'list', or 'exit'."
-      end 
-    end 
-  end 
+  #     if input.to_i > 0 
+  #       the_attraction = @attractions[input.to_i-1]
+  #       puts "#{attraction.name} - #{attraction.type} - #{attraction.price}"
+  #     elsif input == "list" 
+  #       list 
+  #     else 
+  #       puts "" 
+  #       puts "Invalid entry. Please type a number for information on an attraction, 'list', or 'exit'."
+  #       menu 
+  #     end 
+  #   end 
+  # end 
   
-  def goodbye
-    puts "Thank you! We hope to see you return soon for more information on Banff's world-class attractions!"
-  end 
+  # def goodbye
+  #   puts "" 
+  #   puts "Thank you! We hope to see you return soon for more information on Banff's world-class attractions!"
+  #   exit 
+  # end 
   
 end 
